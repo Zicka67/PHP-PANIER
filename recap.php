ec966a103a5a0a36dbbf8801f1222393fc6ba11d
Zicka67 <art.book@hotmail.fr>
2023-01-20 15:53:04 +0100

    création de recap

---
+++ recap.php
@@ -0,0 +1,26 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Recap</title>
+
+</head>
+
+<body>
+
+<?php var_dump($_SESSION); ?>
+
+
+
+</body>
+
+</html>
