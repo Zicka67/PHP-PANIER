1ec3d99be328d79fd489a201056cc0a702217fdd
Zicka67 <art.book@hotmail.fr>
2023-01-20 11:54:46 +0100

    Création du form + commentaires

--- index.php
+++ index.php
@@ -14,6 +14,30 @@
 
 <body>
 
+    <h1>Ajouter un produit</h1>
+    <form action="traitement.php" method="post"> <!-- action => indique la cible du formulaire, le fichier a atteindre quand le user soumet le form -->
+    <p>                                          <!-- method => précise la méthode HTTP de transmission au serveur, des données du form -->
+        <label>
+            Nom du produit :
+            <input type="text" name="name">
+        </label>
+    </p>
+    <p>
+        <label>
+            Prix du produit :
+            <input type="number" step="any" name="price">
+        </label>
+    </p>
+    <p>
+        <label>
+            Quantité désirée :
+            <input type="submit" name="submit" value="Ajouter le produit">
+        </label>
+    </p>
+
+
+
+    </form>
 
 
 
