48772290e4a9e4077796138c6f8df520385700f6
Zicka67 <art.book@hotmail.fr>
2023-01-23 10:02:03 +0100

    Corrections

--- traitement.php
+++ traitement.php
@@ -8,14 +8,14 @@ if(isset($_POST["submit"])) {
     //Ce filtre validera le prix UNIQUEMENT si il est a virgule et FLAG = permet " , " et " . "
     $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
     //Ce filtre ne validera la quantité UNIQUEMENT si le prix est un chiffre entier différent de 0
-    $qtt = filter_input(INPUT_POST, "price", "qtt", FILTER_VALIDATE_INT);
+    $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
     
     if($name && $price && $qtt) {
         $product = [
             "name" => $name,
             "price" => $price,
             "qtt" => $qtt,
-            "total" => $price/$qtt,
+            "total" => $price*$qtt,
         ];
         $_SESSION["products"][] = $product;//[] est un raccourci pour indiquer que nous ajoutons une nouvelle 
         //entrée au futur tab "products" associé a cette clé.
